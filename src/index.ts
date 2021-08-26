e96657c448c135c3d2e7fd814751a084af711056
wentao <wentao@sobey.com>
2021-08-26 15:50:36 +0800

    poker create print

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-let say:string = 'hello'
+type Cards = CardItem[]
 
-console.log(233)
+type CardType = "♥"|"♠"|"♣"|"♦"
+
+type CardItem = {
+    type: CardType,
+    mark: number
+}
+
+function createCard(): Cards {
+    const cards: Cards = [];
+    for (let i = 0; i <= 13; i++) {
+        cards.push({
+            mark: i,
+            type: '♥'
+        })
+        cards.push({
+            mark: i,
+            type: '♠'
+        })
+        cards.push({
+            mark: i,
+            type: '♣'
+        })
+        cards.push({
+            mark: i,
+            type: '♦'
+        })
+    }
+    return cards;
+}
+
+function printCard(cards: Cards){
+    cards.forEach(card => {
+        let str = card.type;
+        if(card.mark <= 10) {
+            str += card.mark
+        }else if(card.mark === 11) {
+            str += "J"
+        }else if(card.mark === 12) {
+            str += "Q"
+        }else if(card.mark === 13) {
+            str += "K"
+        }
+        console.log(str);
+    })
+}
